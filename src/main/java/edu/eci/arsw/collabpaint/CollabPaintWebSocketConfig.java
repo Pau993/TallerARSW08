509adf6b9dccec7bb6e410c855971c8af3000c8e
Paulinguis993 <99148906+Pau993@users.noreply.github.com>
2025-03-25 21:36:24 -0500

    Se arreglo el error de las versiones

--- src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
@@ -20,10 +20,9 @@ package edu.eci.arsw.collabpaint;
  *
  * @author hcadavid
  */
-import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.util.AntPathMatcher;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -31,6 +30,10 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class CollabPaintWebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+    @Autowired
+    public CollabPaintWebSocketConfig() {
+    }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
